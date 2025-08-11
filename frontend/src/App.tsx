6d3e50099b1143ca962c53d5aebe7ff8f9bb26a8
esotooo <edwinsotos2003@gmail.com>
2025-08-11 15:21:55 -0600

    Se crearon las variables para los colores y fuentes que se utilizaran en el proyecto en _variables

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import LoginPage from './pages/LoginPage'
+import MainPage from './pages/MainPage';
 import './scss/App.scss'
 
 function App() {
 
   return (
-    <>
-      <h1>Pagina principal</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Navigate to='/login' replace />} /> {/** Redireccionar a Login */}
+        <Route path='/login' element={<LoginPage />}/>
+        <Route path='/main' element={<MainPage />} />
+      </Routes>
+    </Router>
   )
 }
 
