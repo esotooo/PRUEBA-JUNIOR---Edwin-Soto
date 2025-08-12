495905f2c10e2c5e479c2fab83b0cc9e7834e8e3
esotooo <edwinsotos2003@gmail.com>
2025-08-11 20:10:42 -0600

    Agregando estilos a el login

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,11 +1,15 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
+import { FaEye } from "react-icons/fa"
+import { FaEyeSlash } from "react-icons/fa"
+
 import axios from 'axios'
 
 export default function LoginPage() {
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [hide, setHide] = useState(true)
 
     const navigate = useNavigate()
 
@@ -28,18 +32,21 @@ export default function LoginPage() {
     }
 
 
-
   return (
-    <>
-        <div className="login">
-            <h1 className="">Bienvenido!</h1>
-            <div className="">
-                <p>Inicio de Sesión</p>
-                <div>
-                    <form action="" onSubmit={handleSubmit}>
+    <div className="login">
+        <div className="login__title">
+            <h1>SupplyHub</h1>
+        </div>
+        <div className="login__container">
+                <div className="login__image">
+                    <img src="../../img/suppliersIMG.webp" alt="Login Image" loading="lazy"/>
+                </div>
+                <div className="login__form">
+                    <h2>Gestiona tus proveedores fácil y rápido</h2>
+                    <form onSubmit={handleSubmit}>
 
                         {/* CAMPO CORREO ELECTRONICO */}
-                        <div>
+                        <div className="login__email">
                             <label htmlFor="email">Correo Electrónico</label>
                             <input type="email" id="email" placeholder="Ingrese su correo electrónico" 
                             value={email}
@@ -47,22 +54,24 @@ export default function LoginPage() {
                         </div>
 
                         {/* CAMPO CONTRASEÑA */}
-                        <div>
+                        <div className="login__password">
                             <label htmlFor="password">Contraseña</label>
-                            <div>
-                                <input type="text" id="password" placeholder="Ingrese su contraseña"
+                            <div className="password--hidden">
+                                <input type={hide ? 'password' : 'text'} id="password" placeholder="Ingrese su contraseña"
                                 value={password}
                                 onChange={(e) => setPassword((e.target as HTMLInputElement).value)}/>
+                                <div className="password--eye" onClick={() => setHide(!hide)}>
+                                    {hide ? <FaEye /> : <FaEyeSlash />}
+                                </div>
                             </div>
                         </div>
 
-                        <button >
+                        <button className="login__button">
                             Iniciar Sesión
                         </button>
                     </form>
                 </div>
-            </div>
         </div>   
-    </>
+    </div>
   )
 }
